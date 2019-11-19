2a756d6b69b95422d324afdfddd1a64941901347
Michael Song <54637731+SONGMJ23@users.noreply.github.com>
2019-11-18 19:41:20 -0500

    Update ProjectMain.ino

--- ProjectMain.ino
+++ ProjectMain.ino
@@ -1,17 +1,25 @@
 #include "Project.h"
+// We also have to include an "LCD.h" for LCD display depending on what LCD we use
 
 Project project;
 
 void setup()
 {
-  // Initialize the serial communication with ECG analog:
+  // Initialize the serial communication with the ECG sensor
   Serial.begin(9600);
   pinMode(10, INPUT); // Setup for leads off detection LO +
   pinMode(11, INPUT); // Setup for leads off detection LO -
+  
+  // Initialize the serial communication with Pulse ox
+  ...
+  
+  // Initialize the serial communication with gyro
+  ...
 }
 
 void loop() 
 {
+  // ECG functioning block
   if ((digitalRead(10) == 1)||(digitalRead(11) == 1))
   {
     Serial.println('!'); // No current inputs!
@@ -24,8 +32,17 @@ void loop()
     count++;
     if (count >= SAMPLE_NUMBER)
     {
-      count = 0;
+      count = 0; // Start a new sampling row
     }
-  }
-  delay(20);
+    // ECG waveform's LCD display
+    ...
+  } 
+  
+  // Pulse ox functioning block
+  ...
+      
+  // Gyro functioning block
+  ...
+
+  delay(20); // Interval of display
 }
