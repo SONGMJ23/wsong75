d4c4e032943e8e8c68facdf502a5aedbbf644935
Michael Song <54637731+SONGMJ23@users.noreply.github.com>
2019-11-28 00:38:11 -0500

    Add files via upload

--- ECG_Graphic/src/Project.h
+++ ECG_Graphic/src/Project.h
@@ -1,1 +1,17 @@
-a
+#ifndef __PROJECT__H
+#define __PROJECT__H
+
+#include "Arduino.h"
+
+#define SAMPLE_NUMBER 100 // Sample 100 points of ECG analog signals in a row
+
+class Project
+{
+public:
+  uint16_t getECG(uint8_t pin); // Get the actual values of ECG potentials
+  
+private:   
+  uint8_t valueCount_=255;
+};
+
+#endif
